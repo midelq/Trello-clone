2013680fd87564f9f1bb36a558454c402294527e
midelq <pryst.oresta@gmail.com>
2025-08-12 10:42:47 +0300

    add routing (react-router-dom)

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
-module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  }
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+  ],
+};
   
